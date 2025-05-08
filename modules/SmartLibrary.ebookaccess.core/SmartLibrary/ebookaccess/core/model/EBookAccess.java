6e2ab23db10abaf62ce7bef86c95851dd0e71f4d
daffagrahito <muhammad.daffa23@ui.ac.id>
2025-05-08 22:44:05 +0700

    fix: features_to_module inconsistensies

--- modules/SmartLibrary.ebookaccess.core/SmartLibrary/ebookaccess/core/model/EBookAccess.java
+++ modules/SmartLibrary.ebookaccess.core/SmartLibrary/ebookaccess/core/model/EBookAccess.java
@@ -4,6 +4,6 @@ import vmj.routing.route.VMJExchange;
 import java.util.*;
 
 public interface EBookAccess {
-	org.eclipse.uml2.uml.internal.impl.ClassImpl@56bab228 (name: EBookAccessImpl, visibility: public) (isLeaf: false, isAbstract: false, isFinalSpecialization: false) (isActive: false)org.eclipse.uml2.uml.internal.impl.DependencyImpl@53578774 (name: <unset>, visibility: <unset>)
+	org.eclipse.uml2.uml.internal.impl.ClassImpl@2c3ad0c5 (name: EBookAccessImpl, visibility: public) (isLeaf: false, isAbstract: false, isFinalSpecialization: false) (isActive: false)org.eclipse.uml2.uml.internal.impl.DependencyImpl@45e79b98 (name: <unset>, visibility: <unset>)
 	HashMap<String, Object> toHashMap();
 }
--- modules/SmartLibrary.wishlistmanagement.core/SmartLibrary/wishlistmanagement/core/model/WishlishManagement.java
+++ modules/SmartLibrary.wishlistmanagement.core/SmartLibrary/wishlistmanagement/core/model/WishlishManagement.java
@@ -4,6 +4,6 @@ import vmj.routing.route.VMJExchange;
 import java.util.*;
 
 public interface WishlishManagement {
-	org.eclipse.uml2.uml.internal.impl.ClassImpl@29a79f0e (name: WishlistManagementImpl, visibility: <unset>) (isLeaf: false, isAbstract: false, isFinalSpecialization: false) (isActive: false)org.eclipse.uml2.uml.internal.impl.ClassImpl@3bda70b2 (name: WishlistItemImpl, visibility: <unset>) (isLeaf: false, isAbstract: false, isFinalSpecialization: false) (isActive: false)org.eclipse.uml2.uml.internal.impl.InterfaceImpl@3dc6fca6 (name: WishlistItemImpl, visibility: <unset>) (isLeaf: false, isAbstract: false, isFinalSpecialization: false)
+	org.eclipse.uml2.uml.internal.impl.ClassImpl@19fceb05 (name: WishlistManagementImpl, visibility: <unset>) (isLeaf: false, isAbstract: false, isFinalSpecialization: false) (isActive: false)org.eclipse.uml2.uml.internal.impl.ClassImpl@5703c384 (name: WishlistItemImpl, visibility: <unset>) (isLeaf: false, isAbstract: false, isFinalSpecialization: false) (isActive: false)org.eclipse.uml2.uml.internal.impl.InterfaceImpl@519fbcb1 (name: WishlistItemImpl, visibility: <unset>) (isLeaf: false, isAbstract: false, isFinalSpecialization: false)
 	HashMap<String, Object> toHashMap();
 }
