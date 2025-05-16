71d4f21502263abf4b13a006203b862528836576
Muhammad Raditya Hanif <muhammad.raditya11@ui.ac.id>
2025-05-16 17:35:56 +0700

    fix: community content

--- modules/SmartLibrary.communitycontent.forumthread/SmartLibrary/communitycontent/forumthread/model/CommunityContentImpl.java
+++ modules/SmartLibrary.communitycontent.forumthread/SmartLibrary/communitycontent/forumthread/model/CommunityContentImpl.java
@@ -18,25 +18,22 @@ public class CommunityContentImpl extends CommunityContentDecorator {
 
 	protected String topic;
 	protected String initialPost;
-	protected EEList replies;
 	public CommunityContentImpl() {
         super();
         this.objectName = CommunityContentImpl.class.getName();
     }
     
-    public CommunityContentImpl(String topic, String initialPost, EEList replies) {
+    public CommunityContentImpl(String topic, String initialPost) {
     	super();
 		this.topic = topic;
 		this.initialPost = initialPost;
-		this.replies = replies;
 		this.objectName = CommunityContentImpl.class.getName();
     }
 	
-	public CommunityContentImpl(CommunityContentComponent record, String topic, String initialPost, EEList replies) {
+	public CommunityContentImpl(CommunityContentComponent record, String topic, String initialPost) {
 		super(record);
 		this.topic = topic;
 		this.initialPost = initialPost;
-		this.replies = replies;
 		this.objectName = CommunityContentImpl.class.getName();
 	}
 
@@ -54,23 +51,13 @@ public class CommunityContentImpl extends CommunityContentDecorator {
 	public void setInitialPost(String initialPost) {
 		this.initialPost = initialPost;
 	}
-	public EEList getReplies() {
-		return this.replies;
-	}
-
-	public void setReplies(EEList replies) {
-		this.replies = replies;
-	}
+	
 
 	public void createContent(String topic, String initialPost) {
 		// TODO: implement this method
 	}
 
 	public void updateContent(UUID contentId, String topic, String initialPost) {
-		// TODO: implement this method
-	}
-
-	public void addReply(UUID contentId, String comment) {
 		// TODO: implement this method
 	}
 
--- modules/SmartLibrary.communitycontent.forumthread/SmartLibrary/communitycontent/forumthread/resource/CommunityContentResourceImpl.java
+++ modules/SmartLibrary.communitycontent.forumthread/SmartLibrary/communitycontent/forumthread/resource/CommunityContentResourceImpl.java
@@ -7,85 +7,93 @@ import vmj.routing.route.VMJExchange;
 import SmartLibrary.communitycontent.core.CommunityContentResourceDecorator;
 import SmartLibrary.communitycontent.core.CommunityContentImpl;
 import SmartLibrary.communitycontent.core.CommunityContentResourceComponent;
+import SmartLibrary.communitycontent.core.CommunityContent;
 
 public class CommunityContentResourceImpl extends CommunityContentResourceDecorator {
+	private CommunityContentServiceImpl communityContentServiceImpl;
+
     public CommunityContentResourceImpl (CommunityContentResourceComponent record) {
         super(record);
     }
 
-    // @Restriced(permission = "")
-    @Route(url="call/forumthread/save")
+	@Route(url="call/forumthread/save")
     public List<HashMap<String,Object>> save(VMJExchange vmjExchange){
 		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
-		CommunityContentForumThread communitycontentforumthread = createCommunityContentForumThread(vmjExchange);
-		communitycontentforumthreadRepository.saveObject(communitycontentforumthread);
-		return getAllCommunityContentForumThread(vmjExchange);
+        Map<String, Object> requestBody = vmjExchange.getPayload();
+
+		CommunityContent result = communityContentServiceImpl.createCommunityContent(requestBody);
+		List<HashMap<String, Object>> response = new ArrayList<>();
+        response.add(result.toHashMap());
+        return response;
 	}
 
-    public CommunityContent createCommunityContentForumThread(VMJExchange vmjExchange){
-		String topic = (String) vmjExchange.getRequestBodyForm("topic");
-		String initialPost = (String) vmjExchange.getRequestBodyForm("initialPost");
+    // public CommunityContent createCommunityContentForumThread(VMJExchange vmjExchange){
+	// 	String topic = (String) vmjExchange.getRequestBodyForm("topic");
+	// 	String initialPost = (String) vmjExchange.getRequestBodyForm("initialPost");
 		
-		CommunityContentForumThread communitycontentforumthread = record.createCommunityContentForumThread(vmjExchange);
-		CommunityContentForumThread communitycontentforumthreaddeco = CommunityContentForumThreadFactory.createCommunityContentForumThread("SmartLibrary.forumthread.core.CommunityContentImpl", communitycontentforumthread, contentAuthor, contentID, createdAt, updatedAt,
-		topic, initialPost, replies
-		);
-			return communitycontentforumthreaddeco;
+	// 	CommunityContentForumThread communitycontentforumthread = record.createCommunityContentForumThread(vmjExchange);
+	// 	CommunityContentForumThread communitycontentforumthreaddeco = CommunityContentForumThreadFactory.createCommunityContentForumThread("SmartLibrary.forumthread.core.CommunityContentImpl", communitycontentforumthread, contentAuthor, contentID, createdAt, updatedAt,
+	// 	topic, initialPost, replies
+	// 	);
+	// 		return communitycontentforumthreaddeco;
+	// }
+
+	@Route(url="call/forumthread/create")
+    public HashMap<String,Object> createCommunityContentForumThread(VMJExchange vmjExchange) {
+		if (vmjExchange.getHttpMethod().equals("POST")) {
+		    Map<String, Object> requestBody = vmjExchange.getPayload(); 
+			CommunityContent result = communityContentServiceImpl.createCommunityContent(requestBody, new HashMap<>());
+			return result.toHashMap();
+		} else {
+			return null;
+		}
 	}
 
 
-    public CommunityContent createCommunityContentForumThread(VMJExchange vmjExchange, int id){
-		String topic = (String) vmjExchange.getRequestBodyForm("topic");
-		String initialPost = (String) vmjExchange.getRequestBodyForm("initialPost");
-		CommunityContentForumThread communitycontentforumthread = communitycontentforumthreadRepository.getObject(id);
-		int recordCommunityContentForumThreadId = ((CommunityContentForumThreadDecorator) savedCommunityContentForumThread.getRecord()).getId();
+    // public CommunityContent createCommunityContentForumThread(VMJExchange vmjExchange, int id){
+	// 	String topic = (String) vmjExchange.getRequestBodyForm("topic");
+	// 	String initialPost = (String) vmjExchange.getRequestBodyForm("initialPost");
+	// 	CommunityContentForumThread communitycontentforumthread = Repository.getObject(id);
+	// 	int recordCommunityContentForumThreadId = ((CommunityContentForumThreadDecorator) savedCommunityContentForumThread.getRecord()).getId();
 		
-		communitycontentforumthread = record.createCommunityContentForumThread(vmjExchange);
-		CommunityContentForumThread communitycontentforumthreaddeco = CommunityContentForumThreadFactory.createCommunityContentForumThread("SmartLibrary.forumthread.core.CommunityContentImpl", id, communitycontentforumthread, contentAuthor, contentID, createdAt, updatedAt,
-		topic, initialPost, replies
-		);
-			return communitycontentforumthreaddeco;
-	}
+	// 	communitycontentforumthread = record.createCommunityContentForumThread(vmjExchange);
+	// 	CommunityContentForumThread communitycontentforumthreaddeco = CommunityContentForumThreadFactory.createCommunityContentForumThread("SmartLibrary.forumthread.core.CommunityContentImpl", id, communitycontentforumthread, contentAuthor, contentID, createdAt, updatedAt,
+	// 	topic, initialPost, replies
+	// 	);
+	// 		return communitycontentforumthreaddeco;
+	// }
 
 	// @Restriced(permission = "")
     @Route(url="call/forumthread/update")
-    public HashMap<String, Object> updateCommunityContentForumThread(VMJExchange vmjExchange){
+    public HashMap<String, Object> updateCommunityContentAForumThread(VMJExchange vmjExchange){
 		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
-		int id = Integer.parseInt(idStr);
-		
-		CommunityContentForumThread communitycontentforumthread = communitycontentforumthreadRepository.getObject(id);
-		communitycontentforumthread = createCommunityContentForumThread(vmjExchange, id);
-		
-		communitycontentforumthreadRepository.updateObject(communitycontentforumthread);
-		communitycontentforumthread = communitycontentforumthreadRepository.getObject(id);
-		//to do: fix association attributes
-		
-		return communitycontentforumthread.toHashMap();
+		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+		return communityContentServiceImpl.updateCommunityContent(requestBody);
 		
 	}
 
 	// @Restriced(permission = "")
     @Route(url="call/forumthread/detail")
-    public HashMap<String, Object> getCommunityContentForumThread(VMJExchange vmjExchange){
-		return record.getCommunityContentForumThread(vmjExchange);
+    public HashMap<String, Object> getCommunityContentAForumThread(VMJExchange vmjExchange){
+		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+		return communityContentServiceImpl.getCommunityContent(requestBody);
 	}
 
 	// @Restriced(permission = "")
     @Route(url="call/forumthread/list")
     public List<HashMap<String,Object>> getAllCommunityContentForumThread(VMJExchange vmjExchange){
-		List<CommunityContentForumThread> communitycontentforumthreadList = communitycontentforumthreadRepository.getAllObject("communitycontentforumthread_impl");
-		return transformCommunityContentForumThreadListToHashMap(communitycontentforumthreadList);
+		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+		return communityContentServiceImpl.getAllCommunityContent(requestBody);
 	}
 
-    public List<HashMap<String,Object>> transformCommunityContentForumThreadListToHashMap(List<CommunityContentForumThread> CommunityContentForumThreadList){
+    public List<HashMap<String,Object>> transformCommunityContentForumThreadListToHashMap(List<CommunityContent> list){
 		List<HashMap<String,Object>> resultList = new ArrayList<HashMap<String,Object>>();
-        for(int i = 0; i < CommunityContentForumThreadList.size(); i++) {
-            resultList.add(CommunityContentForumThreadList.get(i).toHashMap());
+        for(int i = 0; i < list.size(); i++) {
+            resultList.add(list.get(i).toHashMap());
         }
 
         return resultList;
@@ -93,15 +101,13 @@ public class CommunityContentResourceImpl extends CommunityContentResourceDecora
 
 	// @Restriced(permission = "")
     @Route(url="call/forumthread/delete")
-    public List<HashMap<String,Object>> deleteCommunityContentForumThread(VMJExchange vmjExchange){
+    public List<HashMap<String,Object>> deleteCommunityContentAForumThread(VMJExchange vmjExchange){
 		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
 		
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
-		int id = Integer.parseInt(idStr);
-		communitycontentforumthreadRepository.deleteObject(id);
-		return getAllCommunityContentForumThread(vmjExchange);
+		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+		return communityContentServiceImpl.deleteCommunityContent(requestBody);
 	}
 
 	public void createContent(String topic, String initialPost) {
