b66863b1267d802a784184c959b80cf94aa98645
Muhammad Raditya Hanif <muhammad.raditya11@ui.ac.id>
2025-05-23 19:22:28 +0700

    fix forumthread

--- src-gen/CommCon/run.bat
+++ src-gen/CommCon/run.bat
@@ -1,4 +1,4 @@
-echo SELECT 'CREATE DATABASE smartlibrary_product_commcon' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smartlibrary_product_commcon') \gexec | psql "postgresql://postgres:asterisk2@localhost"
-for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:asterisk2@localhost/smartlibrary_product_commcon"
+echo SELECT 'CREATE DATABASE smartlibrary_product_commcon' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smartlibrary_product_commcon') \gexec | psql "postgresql://postgres:postgres@localhost"
+for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:postgres@localhost/smartlibrary_product_commcon"
 
 java -cp smartlibrary.product.commcon --module-path smartlibrary.product.commcon -m smartlibrary.product.commcon
